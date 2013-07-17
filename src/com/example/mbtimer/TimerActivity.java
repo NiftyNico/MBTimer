d2952ae2e3562c3f914b90dd934c0b3b83905c1c
Nico Higuera <nicohiguera11@gmail.com>
2013-07-17 11:35:07 -0700

    made dem buttonz

--- src/com/example/mbtimer/TimerActivity.java
+++ src/com/example/mbtimer/TimerActivity.java
@@ -1,15 +1,47 @@
 package com.example.mbtimer;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
-
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 public class TimerActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.timer_activity_layout);
+
+        Button clockIn = (Button) findViewById(R.id.adpButton);
+        Button startTimer = (Button) findViewById(R.id.startTimerButton);
+
+        clockIn.setOnClickListener(new OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                String url = "https://portal.adp.com/";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+
+        startTimer.setOnClickListener(new OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                String url = "https://portal.adp.com/";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+
+
 	}
 
 	@Override
